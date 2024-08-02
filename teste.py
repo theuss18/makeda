90bd0c6d9d876d9de85e8a275669ae7c6c0fd8f0
theuss18 <matheus.goncalves.camargo@escola.pr.gov.br>
2024-08-02 10:03:11 -0300

    add o code

---
+++ teste.py
@@ -0,0 +1,13 @@
+# isso serve para falar com o mundo
+
+
+"""
+este é um comentario.
+"""
+
+"""
+eu quero treinar
+"""
+print("Seja bem-vindo ao Sistema")
+makeda =input("Diga seu nome meu caro")
+print("Bem-vindo,",makeda +"!")
