91e0a52e7fd8479e6d3babe2fda31b88e224c36b
theuss18 <matheus.goncalves.camargo@escola.pr.gov.br>
2024-08-14 09:53:13 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,6 @@
-# isso serve para falar com o mundo
+import time
+
+#isso serve para falar com o mundo
 
 
 """
@@ -9,5 +11,25 @@ este é um comentario.
 eu quero treinar
 """
 print("Seja bem-vindo ao Sistema")
-makeda =input("Diga seu nome meu caro")
+makeda = input("Diga seu nome meu caro")
 print("Bem-vindo,",makeda +"!")
+
+print("Como posso te ajudar  ?")
+
+print("1-jiu-jitsu")
+print("2-muay thay")
+print("3-boxe")
+
+opcao = int(input("Escolha uma opção desejada"))
+
+if opcao == 1:
+    print("jiu-jitsu")
+    
+elif opcao ==2:
+    print("muay thay")
+    
+elif opcao ==3:
+    print("boxe")
+    
+else:
+    print("opção incorreta")
